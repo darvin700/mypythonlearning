9ae1d452f7432d9e0c168fd22be5126575cec437
Darvinroy <darvin.hemilus@gmail.com>
2025-03-22 17:55:55 +0530

    7 commit

--- Darvin_test.py
+++ Darvin_test.py
@@ -1,87 +1,9 @@
-'''
-a = 20
-b= 30
-#print("olda %d oldb %d"%(a,b))
-a = a + b
-b= a - b
-a= a-b
 
-#print(f"newa is {a} \nnewb is {b}")
+#Python Program to Remove the Given Key from a Dictionary
 
-x =40
-y = 50
-#print("oldx %d oldy %d"%(x,y))
-z=x
-x=y
-y=z
-#print(f"newx is {x} \nnewy is {y}")
-
-print(chr(65))
-
-a = 100
-b = 300
-print(f"old_a is {a} old_b is {b}")
-a,b=b,a
-print(a,b)
-
-name={"dar","dar","roy"}
-print(name)
-print(type(name))
-name=(1,2,3)
-print(type(name))
-
-
-n=[1,2]
-print(type(n))
-n.append(6)
-print(n)
-
-a=10
-b=10
-#a=a+8+b
-print(f"a value is {a}")
-print(id(b),id(a))
-
-
-i="Python"
-j="python"
-if i is j:
-    print(True)
-    print(id(i), id(j))
-else:
-    print(False)
-    print(id(i),id(j))
-
-b="ball"
-print("b value is %s" %b)
-
-
-
-
-print (5*"python")
-print ('5'+"python")
-print (3%4)
-x=-10
-print(f"value of x is {abs(x)}")
-print(f"value of x is {x}")
-
-print (2**-3)
-
-name="darvinroy"
-print(f" display from {name[2:6:1]}")
-
-print(name.upper())
-
-print(type(name))
-print(dir(name))
-print(help(str))
-print(dir(__builtins__))
-
-'''
-
-a = [100, 1, 200, 2, 3, "python", "jaava", [1, 2], "python"]
-for i in range(a.count("python")):
-    a.remove("python")
-a.remove("yyy")
+a={1: 'a', 2: 'b', 'python': 'c', 'a': 1, 'b': 2}
+b = "b"
+if b in a:
+    del a[b]
 
 print(a)
---
+++ Python-Learning-master/Assignments/set_dict.py
@@ -0,0 +1,116 @@
+#Remove duplicate values of a list
+a=[1,2,3,4,1,2,3,10]
+#a=set(a)
+#a=list(a)
+print(a)
+a=list(set(a))
+print(a)
+
+
+s2 = set( ['abc', 12, 'b', 'car', 7, 10, 12 ])
+s2 = set( ['abc', 12, 'b', 'car', 7, 10, 12 ])
+s3 = set( [12, 14, 12, 'ab'] )
+b=list(s2)
+
+#print b in s2
+
+for i in b:
+    if i == "b":
+        print(f"value is {i}")
+
+j=len(b)
+for i in b:
+    j-=1
+    if i == "ab":
+        print(f"value is {i}")
+        break
+    elif j == 0:
+        print("ab is not in list s2")
+
+s2.discard(12)
+print(s2)
+
+#assignment
+s = (s1 ^ s2 ^ s3) - ((s1 & s2) | (s2 & s3) | (s1 & s3))
+print(s)
+
+#symetric
+
+n="12345"
+m=123,13,45
+
+n=set(n)
+m=set(m)
+
+print(m,n)
+print((n^m))
+
+
+
+#Take a user input as a animal name and return the height of that animal
+heights={'belgian horse': 162,'tiger': 168, 'cat': 58, 'dog': 57, 'cow': 59, 'lion': 109}
+s=input("Enter animal name(hors,dog,tiger,cat,cow,lion):")
+for i in heights:
+    if i == s:
+        print(heights[i])
+        break
+
+
+
+# initializing Dictionary
+d = { 'a' : 1 , 'b' : 2 }
+print ("The value associated with 'c' is : ")
+d['c']=5
+print (d['c'])
+print(" learning python is fun")
+print(" python is awsome")
+
+#change the key,valye
+
+d = { 'a' : 1 , 'b' : 2,'c': 'python' }
+l=[]
+k=[]
+#print(d.items())
+for key,value in d.items():
+    l.append(key)
+    k.append(value)
+m=zip(k,l)
+print(dict(m))
+
+#merge teo dict
+a= {1: 'a', 2: 'b', 'python': 'c'}
+b={ 'a' : 1 , 'b' : 2 }
+c={}
+a.update(b)
+print(a)
+
+#Python Program to Check if a Given Key Exists in a Dictionary or Not*
+a={1: 'a', 2: 'b', 'python': 'c', 'a': 1, 'b': 2}
+print(a.get("d","it does not exist"))
+
+#Python Program to Multiply All the Items in a Dictionary
+d = { 'a' : 10 , 'b' : 20 }
+d["c"]=10
+k=1
+print(d)
+for i,j in d.items():
+    print(j)
+    k=k*j
+print(k)
+
+
+#Python Program to Remove the Given Key from a Dictionary
+a={1: 'a', 2: 'b', 'python': 'c', 'a': 1, 'b': 2}
+a.pop("python")
+print(a)
+
+#Python Program to Remove the Given Key from a Dictionary
+
+a={1: 'a', 2: 'b', 'python': 'c', 'a': 1, 'b': 2}
+b = "b"
+if b in a:
+    del a[b]
+
+print(a)
+
+
